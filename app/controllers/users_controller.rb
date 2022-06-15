bdd1416bf602bd25e741f0ee986884f29bb997c9
C3lt1c-Viking <c3lt1cviking@tuta.io>
2022-06-15 09:57:33 -0500

    Removed activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome to OctaneDeck!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
