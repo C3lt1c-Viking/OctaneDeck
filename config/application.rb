80c5f4af25b4e101a32308c8b892604d6514bf70
C3lt1c-Viking <c3lt1cviking@tuta.io>
2022-06-14 22:09:18 -0500

    Working on Decryption Error Issues v-1.1

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module CryptoDeck
     config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
+    secret_key_base: b26e94eade57cde374e7cbd0746daf4c6e4801a2507623b073bdba553c3f8866312d7cca4b37315c57a1f30adf406898009a39c200549647d14e3a675802a577
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
