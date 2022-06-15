fcf5a3583880a2707c03b0fce6b40704b574b76f
C3lt1c-Viking <c3lt1cviking@tuta.io>
2022-06-15 10:04:52 -0500

    Removed activation v-1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token, :reset_token
+  attr_accessor :remember_token, :reset_token
   before_save   :downcase_email
-  before_create :create_activation_digest
 
   has_many :microposts, dependent: :destroy
 
@@ -133,17 +132,6 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
-  # Activates an account.
-  def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
-
-  # Sends activation email.
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
-
 
   # Sets the password reset attributes.
   def create_reset_digest
@@ -169,12 +157,5 @@ class User < ApplicationRecord
       def downcase_email
         self.email = email.downcase
       end
-
-      # Creates and assigns the activation token and digest.
-      def create_activation_digest
-        self.activation_token  = User.new_token
-        self.activation_digest = User.digest(activation_token)
-      end
-
 
 end
