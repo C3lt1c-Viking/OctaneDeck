565a26488694a39f53e5433299579a22e5c7031d
C3lt1c-Viking <c3lt1cviking@tuta.io>
2022-06-15 09:28:19 -0500

    Working on Removing/Adding Account Activation v-1.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://cryptodeck.herokuapp.com/'
+  host = 'https://octanedeck.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
